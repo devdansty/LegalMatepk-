764ca9df7b278d7a7ebe827d492e19b36439b0cb
MHS391 <mdharoon691@gmail.com>
2026-03-03 00:30:02 +0500

    created seperate file for base routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import lawyerRoutes from "./modules/lawyers/lawyer.routes.js";
 import chatbotModule from "./modules/chatbot/index.js";
 import userRoutes from "./modules/users/user.routes.js";
 import sessionRoutes from "./modules/sessions/session.routes.js";
@@ -20,5 +21,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/sessions", sessionRoutes);
 
 app.get("/health", (req, res) => res.json({ ok: true }));
+
+app.use("/api/lawyers", lawyerRoutes);
 
 export default app;
